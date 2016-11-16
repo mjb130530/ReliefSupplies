b61c031d73d0e5d3e8fff712a5a1824acf15e2a2
Toadens <bostonjohn94@gmail.com>
2016-11-15 20:47:25 -0600

    testing

---
+++ PopulateLogins.sql
@@ -0,0 +1,11 @@
+INSERT INTO logins (JAMusername, JAMpassword, JAMemail, JAMphone, JAMpermission)
+	VALUES ('admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'admin@admin.com', '1234567890', '1');
+    
+INSERT INTO logins (JAMusername, JAMpassword, JAMemail, JAMphone, JAMpermission)
+	VALUES ('Johnny', '9deb05d9d33d144e9dd9cc21465fc01a036077176fdd99f3c58c827df0cf6d6d', 'johnny@edgett.com', '1234567890', '2');
+    
+INSERT INTO logins (JAMusername, JAMpassword, JAMemail, JAMphone, JAMpermission)
+	VALUES ('Matthew', 'accb7fc4d0144dcaa760e485a8100fc977f3404d3bfdc8e589384f66ca934330', 'matthew@butler.com', '1234567890', '2');
+    
+INSERT INTO logins (JAMusername, JAMpassword, JAMemail, JAMphone, JAMpermission)
+	VALUES ('Abdul', '955a216806c6be47579385a468437c751ac4de8e01dfdd91382675c31cb87e6b', 'abdul@wahab.com', '1234567890', '2');
---
+++ ReadMe.txt
@@ -0,0 +1,6 @@
+The usernames and passwords are currently our first names and last names, as well as an admin account (admin/admin)
+So, (Johnny/Edgett), (Matthew/Butler), (Abdul/Wahab)
+
+email is first@last.com like johnny@edgett.com
+
+phone number for all is 1234567890
